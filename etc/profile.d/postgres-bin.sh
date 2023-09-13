1758ee495b1dd7f645cd068e4f1451dee593260b
CDI Staff member <lauri@korc.jp>
2023-09-13 13:43:31 +0900

    fix path with multiple versions, set manpath

--- etc/profile.d/postgres-bin.sh
+++ etc/profile.d/postgres-bin.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
 which psql > /dev/null || {
-  for _d in $(echo /opt/postgres/lib/postgresql/*/bin | tr ' ' '\n' | sort -nr);do
-    test ! -x "$_d/psql" -a ! -x "$_d/postgres" || {
-      case "$PATH" in
-        *:$_d|$_d:*|*:$_d:*) ;;
-        *) export PATH="$PATH:$_d" ;;
+  for _pg_dir in $(find /opt/postgres/lib/postgresql -maxdepth 2 -name bin | sort -V | tac);do
+    test ! -x "$_pg_dir/psql" -a ! -x "$_pg_dir/postgres" || {
+      case ":$PATH:" in
+        *:"$_pg_dir":*) ;;
+        *) export PATH="$PATH:$_pg_dir" ;;
       esac
     }
   done
+  for _pg_dir in $(find /opt/postgres/share/postgresql -maxdepth 2 -name man | sort -V | tac);do
+    test ! -e "$_pg_dir/man1/psql.1.gz" || {
+      case ":$MANPATH:" in
+        *:"$_pg_dir":*) ;;
+        *) export MANPATH="${MANPATH:-:}:$_pg_dir" ;;
+      esac
+    }
+  done
+  unset _pg_dir
 }
